f2a99bcf0b2873fce75b0ead6179e853c513cf25
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-04 20:06:48 -0300

    chore: add path mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteTsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    extensions: ['.scss', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    ViteTsconfigPaths({ loose: true }),
+    react()
+  ],
 })
