300ecb36a1457d3cbc3a8cbc0462ea6ebc02c06c
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-06 13:24:28 -0300

    feat: add validation for email

--- src/components/email-input/index.tsx
+++ src/components/email-input/index.tsx
@@ -1,7 +1,30 @@
-import { FormControl, InputLabel, FilledInput, InputAdornment, IconButton } from '@mui/material';
+import { useState } from 'react';
+import { FormControl, InputLabel, FilledInput, InputAdornment, IconButton, TextField } from '@mui/material';
 import { Email } from '@mui/icons-material';
 
 function EmailInput() {
+  const [email, setEmail] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const validateEmail = (email: string): string => {
+    const regex = /\S+@\S+\.\S+/;
+
+    if (!email) {
+      return 'E-mail é obrigatório';
+    }
+
+    if (!regex.test(email)) {
+      return 'E-mail inválido';
+    }
+
+    return '';
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+    setError(validateEmail(event.target.value));
+  };
+
   return (
     <FormControl
       margin="normal"
@@ -16,6 +39,8 @@ function EmailInput() {
       <FilledInput
         id="outlined-adornment-email"
         color="success"
+        onChange={handleChange}
+        value={email}
         endAdornment={
           <InputAdornment position="end">
             <IconButton
@@ -26,6 +51,7 @@ function EmailInput() {
           </InputAdornment>
         }
       />
+      { error !== '' && <p className='error'>{ error }</p>}
     </FormControl>
   );
 }
