4b08946f30de03d39f12b4da401ac734aa23f7c3
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-07 10:06:59 -0300

    feat: create sign up button

---
+++ src/components/signup-button/index.tsx
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button';
+
+const SignUpButton = () => (
+  <span style={{ margin: '16px 0' }}>
+    <Button
+      className="button"
+      variant="text"
+      color="primary"
+      size="small"
+    >
+      Criar conta
+    </Button>
+  </span>
+)
+
+export default SignUpButton;
