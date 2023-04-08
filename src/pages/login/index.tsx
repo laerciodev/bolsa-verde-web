21eb3f8706068b1781bcadb4f909ed4ad04548e5
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-08 10:53:44 -0300

    feat: add container-page class

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,12 +2,11 @@ import EmailInput from '@/components/email-input';
 import PasswordInput from '@/components/password-input';
 import Button from '@/components/button';
 import SignUpButton from '@/components/signup-button';
-import Styles from './styles.module.scss';
 
 function Login() {
 
   return (
-    <div className={Styles.container}>
+    <div className="container-page">
       <EmailInput />
       <PasswordInput />
       <Button />
