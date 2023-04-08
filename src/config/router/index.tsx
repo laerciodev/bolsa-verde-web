00971642173d8d23069b5297ca41b7acd274c464
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-08 10:55:46 -0300

    refactor: remove RouterProvider import

--- src/config/router/index.tsx
+++ src/config/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Login from '@pages/login';
 import SignUp from '@pages/signup';
 
