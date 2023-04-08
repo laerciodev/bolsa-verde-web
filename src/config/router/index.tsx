1e0ccefb7604226953c681b724a3bf2437e6d57b
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-08 09:32:29 -0300

    feat: add router config

---
+++ src/config/router/index.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from '@pages/login';
+import SignUp from '@pages/signup';
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  }
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '@/styles/global.scss';
-
-import App from './App'
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@/config/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
