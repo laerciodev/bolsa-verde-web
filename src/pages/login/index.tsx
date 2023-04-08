701a9b5b3fc087a6409aa0f712700eee58dc9ad4
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-08 16:30:38 -0300

    feat: add submit function in login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import EmailInput from '@/components/email-input';
 import PasswordInput from '@/components/password-input';
 import Button from '@/components/button';
@@ -5,11 +6,37 @@ import SignUpButton from '@/components/signup-button';
 
 function Login() {
 
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  function sendPassword(password: string) {
+    setPassword(password);
+  }
+
+  function sendEmail(email: string) {
+    setEmail(email);
+  }
+
+  function submit() {
+    if (!email || !password) {
+      return;
+    }
+
+    if (email === 'user@email.com' && password === '123456') {
+      console.log('login feito com sucesso');
+    } else {
+      console.log('usuário ou senha inválidos');
+    }
+  }
+
   return (
     <div className="container-page">
-      <EmailInput />
-      <PasswordInput />
-      <Button name="Entrar" />
+      <EmailInput sendEmail={sendEmail} />
+      <PasswordInput sendPassword={sendPassword} />
+      <Button
+        name="Entrar"
+        onclick={submit}
+      />
       <SignUpButton />
     </div>
   )
