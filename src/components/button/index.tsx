b5cf2c18bed83a14817a188a52c9a9cbce37c782
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-08 16:29:57 -0300

    feat: add onClick prop in button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,6 +3,7 @@ import Styles from './styles.module.scss';
 
 interface BaseButtonProps {
   name: string;
+  onclick?: () => void;
 }
 
 function BaseButton(props: BaseButtonProps) {
@@ -15,6 +16,7 @@ function BaseButton(props: BaseButtonProps) {
         color="success"
         size="large"
         className={Styles.button}
+        onClick={props.onclick}
       >
         { props.name }
       </Button>
