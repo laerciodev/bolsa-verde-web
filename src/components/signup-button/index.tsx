5054a2c448e245ed61a3a1b0f807d1e5fc7aa446
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-08 10:51:38 -0300

    feat: add useNavigate in signup-button component

--- src/components/signup-button/index.tsx
+++ src/components/signup-button/index.tsx
@@ -1,16 +1,27 @@
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
-const SignUpButton = () => (
-  <span style={{ margin: '16px 0' }}>
-    <Button
-      className="button"
-      variant="text"
-      color="primary"
-      size="small"
-    >
-      Criar conta
-    </Button>
-  </span>
-)
+const SignUpButton = () => {
+
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/signup');
+  }
+
+  return (
+    <span style={{ margin: '16px 0' }}>
+      <Button
+        className="button"
+        variant="text"
+        color="primary"
+        size="small"
+        onClick={handleClick}
+      >
+        Criar conta
+      </Button>
+    </span>
+  )
+}
 
 export default SignUpButton;
