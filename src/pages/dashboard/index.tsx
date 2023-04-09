af42714c81f9b7c6e9282f5716e607b0c0ae2f15
Laércio <jose.laercio.bezerra@gmail.com>
2023-04-09 10:13:08 -0300

    feat: render companies

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,55 @@
 import SearchInput from "@/components/search-input";
+import Company from "@/components/company";
+import Styles from './styles.module.scss';
 
 const Dashboard = () => {
+  const companies = [
+    {
+      name: 'Banco do Brasil',
+      price: 20.00,
+      logo: 'banco-do-brasil',
+    },
+    {
+      name: 'Petrobrás',
+      price: 25.00,
+      logo: 'petrobras',
+    },
+    {
+      name: 'Nubank',
+      price: 5.00,
+      logo: 'nubank',
+    },
+    {
+      name: 'Itaú',
+      price: 25.00,
+      logo: 'itau',
+    },
+    {
+      name: 'Magalu',
+      price: 25.00,
+      logo: 'magalu',
+    }
+  ];
+
+  const renderCompanies = () => {
+    return companies.map((company, index) => {
+      return (
+        <Company
+          key={index}
+          name={company.name}
+          price={company.price}
+          logo={company.logo}
+        />
+      );
+    });
+  };
+
   return (
     <div style={{ margin: '16px' }}>
       <SearchInput />
+      <div className={Styles.companiesContainer} >
+        { renderCompanies() }
+      </div>
     </div>
   );
 };
